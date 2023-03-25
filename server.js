24903ce6be043e13bc57688e3536f6b058ef4480
LogicKahanHai <contact.logicnotfound@gmail.com>
2023-03-25 20:16:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ app.use(express.json());
 app.use(errorHandler);
 
 app.get('/', (req, res) => {
-    res.send().json({
-        message: 'Welcome to the LPUEats API',
-        version: '0.0.1',
-        api_routes_currently_up: {
-            register: '/api/dev/customers/register',
-            login: '/api/dev/customers/login',
-            logout: '/api/dev/customers/logout',
-            profile: '/api/dev/customers/profile',
-            updatePassword: '/api/dev/customers/password'
+    res.send({
+        'message': 'Welcome to the LPUEats API',
+        'version': '0.0.1',
+        'api_routes_currently_up': {
+            'register': '/api/dev/customers/register',
+            'login': '/api/dev/customers/login',
+            'logout': '/api/dev/customers/logout',
+            'profile': '/api/dev/customers/profile',
+            'updatePassword': '/api/dev/customers/password'
         }
     });
 });
